2072c3a3ace0d97b14cd65b5db5afbe9981192a5
Mahesh Khadatare <mkhadatare@nvidia.com>
2021-08-19 11:04:53 -0700

    cuTENSOR 1.3.1: einsum sample + python (einsum) bindings

---
+++ cuTENSOR/python/cutensor/torch/einsum.cc
@@ -0,0 +1,87 @@
+/*  
+ * Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+ * 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  - Neither the name(s) of the copyright holder(s) nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */  
+
+#include <ATen/ATen.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <THC/THC.h>
+#include <torch/extension.h>
+
+#include <cuda.h>
+#include <cuda_fp16.hpp>
+
+#include "../../einsum.h"
+
+template<>
+struct CuTensorTypeTraits<at::Half> {
+  static const cudaDataType_t cudaType = CUDA_R_16F;
+  static const cutensorComputeType_t cutensorType = CUTENSOR_R_MIN_32F;
+  typedef float ScalarType;
+};
+
+template<>
+struct CuTensorTypeTraits<at::BFloat16> {
+  static const cudaDataType_t cudaType = CUDA_R_16BF;
+  static const cutensorComputeType_t cutensorType = CUTENSOR_R_MIN_32F;
+  typedef float ScalarType;
+};
+
+torch::Tensor einsum(
+    std::string subscripts,
+    torch::Tensor input_0,
+    torch::Tensor input_1
+) {
+  at::Tensor output_tensor;
+  AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, input_0.scalar_type(), "einsum", [&] {
+    constexpr int kMaxNumModes_ = 40; // maximal number of modes supported by cuTENSOR
+    Einsum<scalar_t, int64_t, kMaxNumModes_> myEinsum(subscripts, input_0.sizes().vec(), input_1.sizes().vec());
+    if (!myEinsum.isInitialized()) {
+      throw std::runtime_error("cutensor: Initialization failed.");
+    }
+
+    output_tensor = torch::empty(myEinsum.getOutputShape(), input_0.options());
+
+    size_t worksize = myEinsum.getWorksize();
+    at::Tensor workspace = at::empty({static_cast<int>(worksize)}, at::CUDA(at::kByte));
+
+    auto stream = at::cuda::getCurrentCUDAStream().stream();
+    auto ret = myEinsum.execute(GetCuTensorHandle(),
+                                input_0.data_ptr<scalar_t>(),
+                                input_1.data_ptr<scalar_t>(),
+                                output_tensor.data_ptr<scalar_t>(),
+                                workspace.data_ptr<uint8_t>(),
+                                stream);
+
+    if (! ret) throw std::runtime_error("cutensor: Launch failed.");
+  });
+  return output_tensor;
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("einsum", &einsum, "Einsum");
+}
