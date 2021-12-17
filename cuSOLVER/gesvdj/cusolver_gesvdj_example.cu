4256ff7f75c5eae699ea531c10906c881896926e
Matthew Nicely <mnicely@nvidia.com>
2021-12-16 20:10:27 -0800

    add gesvdj and gesvdjBatched example

--- cuSOLVER/gesvdj/cusolver_gesvdj_example.cu
+++ cuSOLVER/gesvdj/cusolver_gesvdj_example.cu
@@ -62,91 +62,54 @@ int main(int argc, char *argv[]) {
     cudaStream_t stream = NULL;
     gesvdjInfo_t gesvdj_params = NULL;
 
-    const int m = 3;   /* 1 <= m <= 32 */
-    const int n = 2;   /* 1 <= n <= 32 */
-    const int lda = m; /* lda >= m */
-    const int ldu = m; /* ldu >= m */
-    const int ldv = n; /* ldv >= n */
-    const int batchSize = 2;
+    const int m = 3;                   /* 1 <= m <= 32 */
+    const int n = 2;                   /* 1 <= n <= 32 */
+    const int lda = m;                 /* lda >= m */
+    const int ldu = m;                 /* ldu >= m */
+    const int ldv = n;                 /* ldv >= n */
     const int minmn = (m < n) ? m : n; /* min(m,n) */
 
     /*
-     *        |  1  -1  |
-     *   A0 = | -1   2  |
-     *        |  0   0  |
-     *
-     *   A0 = U0 * S0 * V0**T
-     *   S0 = diag(2.6180, 0.382)
-     *
-     *        |  3   4  |
-     *   A1 = |  4   7  |
-     *        |  0   0  |
-     *
-     *   A1 = U1 * S1 * V1**T
-     *   S1 = diag(9.4721, 0.5279)
+     *    | 1 2  |
+     *   A = | 4 5  |
+     *       | 2 1  |
      */
 
-    std::vector<double> A(lda * n * batchSize, 0); /* A = [A0 ; A1] */
-    std::vector<double> U(ldu * m * batchSize, 0); /* U = [U0 ; U1] */
-    std::vector<double> V(ldv * n * batchSize, 0); /* V = [V0 ; V1] */
-    std::vector<double> S(minmn * batchSize, 0);   /* S = [S0 ; S1] */
-    std::vector<int> info(batchSize, 0);             /* info = [info0 ; info1] */
+    std::vector<double> A = {1.0, 4.0, 2.0, 2.0, 5.0, 1.0};
+    std::vector<double> U(ldu * m, 0); /* m-by-m unitary matrix, left singular vectors  */
+    std::vector<double> V(ldv * n, 0); /* n-by-n unitary matrix, right singular vectors */
+    std::vector<double> S(minmn, 0);   /* numerical singular value */
+    std::vector<double> S_exact = {7.065283497082729,
+                                   1.040081297712078}; /* exact singular values */
+    int info = 0;                                      /* host copy of error info */
 
-    double *d_A = nullptr; /* lda-by-n-by-batchSize */
-    double *d_U = nullptr; /* ldu-by-m-by-batchSize */
-    double *d_V = nullptr; /* ldv-by-n-by-batchSize */
-    double *d_S = nullptr; /* minmn-by-batchSize */
-    int *d_info = nullptr; /* batchSize */
+    double *d_A = nullptr;
+    double *d_S = nullptr; /* singular values */
+    double *d_U = nullptr; /* left singular vectors */
+    double *d_V = nullptr; /* right singular vectors */
+
+    int *d_info = nullptr;
 
     int lwork = 0;            /* size of workspace */
     double *d_work = nullptr; /* device workspace for getrf */
 
+    /* configuration of gesvdj  */
     const double tol = 1.e-7;
     const int max_sweeps = 15;
-    const int sort_svd = 0;                                  /* don't sort singular values */
-    const cusolverEigMode_t jobz = CUSOLVER_EIG_MODE_VECTOR; /* compute singular vectors */
+    const cusolverEigMode_t jobz = CUSOLVER_EIG_MODE_VECTOR; // compute eigenvectors.
+    const int econ = 0;                                      /* econ = 1 for economy size */
 
-    double *A0 = A.data();
-    double *A1 = A.data() + lda * n; /* Aj is m-by-n */
-
-    /*
-     *        |  1  -1  |
-     *   A0 = | -1   2  |
-     *        |  0   0  |
-     *   A0 is column-major
-     */
-    A0[0 + 0 * lda] = 1.0;
-    A0[1 + 0 * lda] = -1.0;
-    A0[2 + 0 * lda] = 0.0;
-
-    A0[0 + 1 * lda] = -1.0;
-    A0[1 + 1 * lda] = 2.0;
-    A0[2 + 1 * lda] = 0.0;
-
-    /*
-     *        |  3   4  |
-     *   A1 = |  4   7  |
-     *        |  0   0  |
-     *   A1 is column-major
-     */
-    A1[0 + 0 * lda] = 3.0;
-    A1[1 + 0 * lda] = 4.0;
-    A1[2 + 0 * lda] = 0.0;
-
-    A1[0 + 1 * lda] = 4.0;
-    A1[1 + 1 * lda] = 7.0;
-    A1[2 + 1 * lda] = 0.0;
+    /* numerical results of gesvdj  */
+    double residual = 0;
+    int executed_sweeps = 0;
 
     std::printf("m = %d, n = %d \n", m, n);
     std::printf("tol = %E, default value is machine zero \n", tol);
     std::printf("max. sweeps = %d, default value is 100\n", max_sweeps);
+    std::printf("econ = %d \n", econ);
 
-    std::printf("A0 = (matlab base-1)\n");
+    std::printf("A = (matlab base-1)\n");
     print_matrix(m, n, A.data(), lda, CUBLAS_OP_T);
-    std::printf("=====\n");
-
-    std::printf("A1 = (matlab base-1)\n");
-    print_matrix(m, n, A.data() + lda * n, lda, CUBLAS_OP_T);
     std::printf("=====\n");
 
     /* step 1: create cusolver handle, bind a stream */
@@ -164,29 +127,55 @@ int main(int argc, char *argv[]) {
     /* default value of max. sweeps is 100 */
     CUSOLVER_CHECK(cusolverDnXgesvdjSetMaxSweeps(gesvdj_params, max_sweeps));
 
-    /* disable sorting */
-    CUSOLVER_CHECK(cusolverDnXgesvdjSetSortEig(gesvdj_params, sort_svd));
-
     /* step 3: copy A to device */
     CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_A), sizeof(double) * A.size()));
+    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_S), sizeof(double) * S.size()));
     CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_U), sizeof(double) * U.size()));
     CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_V), sizeof(double) * V.size()));
-    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_S), sizeof(double) * S.size()));
-    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_info), sizeof(int) * info.size()));
+    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_info), sizeof(int)));
 
     CUDA_CHECK(
-        cudaMemcpyAsync(d_A, A.data(), sizeof(double) * A.size(), cudaMemcpyHostToDevice, stream));
+        cudaMemcpyAsync(d_A, A.data(), sizeof(double) * lda * n, cudaMemcpyHostToDevice, stream));
 
-    /* step 4: query working space of gesvdjBatched */
-    CUSOLVER_CHECK(cusolverDnDgesvdjBatched_bufferSize(cusolverH, jobz, m, n, d_A, lda, d_S, d_U,
-                                                       ldu, d_V, ldv, &lwork, gesvdj_params,
-                                                       batchSize));
+    /* step 4: query working space of SVD */
+    CUSOLVER_CHECK(cusolverDnDgesvdj_bufferSize(
+        cusolverH, jobz, /* CUSOLVER_EIG_MODE_NOVECTOR: compute singular values only */
+                         /* CUSOLVER_EIG_MODE_VECTOR: compute singular value and singular vectors */
+        econ,            /* econ = 1 for economy size */
+        m,               /* nubmer of rows of A, 0 <= m */
+        n,               /* number of columns of A, 0 <= n  */
+        d_A,             /* m-by-n */
+        lda,             /* leading dimension of A */
+        d_S,             /* min(m,n) */
+                         /* the singular values in descending order */
+        d_U,             /* m-by-m if econ = 0 */
+                         /* m-by-min(m,n) if econ = 1 */
+        ldu,             /* leading dimension of U, ldu >= max(1,m) */
+        d_V,             /* n-by-n if econ = 0  */
+                         /* n-by-min(m,n) if econ = 1  */
+        ldv,             /* leading dimension of V, ldv >= max(1,n) */
+        &lwork, gesvdj_params));
 
     CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_work), sizeof(double) * lwork));
 
-    /* step 5: compute singular values of A0 and A1 */
-    CUSOLVER_CHECK(cusolverDnDgesvdjBatched(cusolverH, jobz, m, n, d_A, lda, d_S, d_U, ldu, d_V,
-                                            ldv, d_work, lwork, d_info, gesvdj_params, batchSize));
+    /* step 5: compute SVD*/
+    CUSOLVER_CHECK(cusolverDnDgesvdj(
+        cusolverH, jobz, /* CUSOLVER_EIG_MODE_NOVECTOR: compute singular values only */
+                         /* CUSOLVER_EIG_MODE_VECTOR: compute singular value and singular vectors */
+        econ,            /* econ = 1 for economy size */
+        m,               /* nubmer of rows of A, 0 <= m */
+        n,               /* number of columns of A, 0 <= n  */
+        d_A,             /* m-by-n */
+        lda,             /* leading dimension of A */
+        d_S,             /* min(m,n)  */
+                         /* the singular values in descending order */
+        d_U,             /* m-by-m if econ = 0 */
+                         /* m-by-min(m,n) if econ = 1 */
+        ldu,             /* leading dimension of U, ldu >= max(1,m) */
+        d_V,             /* n-by-n if econ = 0  */
+                         /* n-by-min(m,n) if econ = 1  */
+        ldv,             /* leading dimension of V, ldv >= max(1,n) */
+        d_work, lwork, d_info, gesvdj_params));
 
     CUDA_CHECK(
         cudaMemcpyAsync(U.data(), d_U, sizeof(double) * U.size(), cudaMemcpyDeviceToHost, stream));
@@ -194,52 +183,45 @@ int main(int argc, char *argv[]) {
         cudaMemcpyAsync(V.data(), d_V, sizeof(double) * V.size(), cudaMemcpyDeviceToHost, stream));
     CUDA_CHECK(
         cudaMemcpyAsync(S.data(), d_S, sizeof(double) * S.size(), cudaMemcpyDeviceToHost, stream));
-    CUDA_CHECK(cudaMemcpyAsync(info.data(), d_info, sizeof(int) * info.size(),
-                               cudaMemcpyDeviceToHost, stream));
+    CUDA_CHECK(cudaMemcpyAsync(&info, d_info, sizeof(int), cudaMemcpyDeviceToHost, stream));
 
     CUDA_CHECK(cudaStreamSynchronize(stream));
 
-    for (int i = 0; i < batchSize; i++) {
-        if (0 == info[i]) {
-            std::printf("matrix %d: gesvdj converges \n", i);
-        } else if (0 > info[i]) {
-            /* only info[0] shows if some input parameter is wrong.
-             * If so, the error is CUSOLVER_STATUS_INVALID_VALUE.
-             */
-            std::printf("Error: %d-th parameter is wrong \n", -info[i]);
-            exit(1);
-        } else { /* info = m+1 */
-                 /* if info[i] is not zero, Jacobi method does not converge at i-th matrix. */
-            std::printf("WARNING: matrix %d, info = %d : gesvdj does not converge \n", i, info[i]);
-        }
+    if (0 == info) {
+        std::printf("gesvdj converges \n");
+    } else if (0 > info) {
+        std::printf("%d-th parameter is wrong \n", -info);
+        exit(1);
+    } else {
+        std::printf("WARNING: info = %d : gesvdj does not converge \n", info);
     }
 
-    /* Step 6: show singular values and singular vectors */
-    double *S0 = S.data();
-    double *S1 = S.data() + minmn;
-    std::printf("==== \n");
-    for (int i = 0; i < minmn; i++) {
-        std::printf("S0(%d) = %20.16E\n", i + 1, S0[i]);
-    }
-    std::printf("==== \n");
-    for (int i = 0; i < minmn; i++) {
-        std::printf("S1(%d) = %20.16E\n", i + 1, S1[i]);
-    }
-    std::printf("==== \n");
+    std::printf("S = singular values (matlab base-1)\n");
+    print_matrix(minmn, 1, S.data(), minmn);
+    std::printf("=====\n");
 
-    double *U0 = U.data();
-    double *U1 = U.data() + ldu * m; /* Uj is m-by-m */
-    std::printf("U0 = (matlab base-1)\n");
-    print_matrix(m, m, U0, ldu);
-    std::printf("U1 = (matlab base-1)\n");
-    print_matrix(m, m, U1, ldu);
+    std::printf("U = left singular vectors (matlab base-1)\n");
+    print_matrix(m, m, U.data(), ldu);
+    std::printf("=====\n");
 
-    double *V0 = V.data();
-    double *V1 = V.data() + ldv * n; /* Vj is n-by-n */
-    std::printf("V0 = (matlab base-1)\n");
-    print_matrix(n, n, V0, ldv);
-    std::printf("V1 = (matlab base-1)\n");
-    print_matrix(n, n, V1, ldv);
+    std::printf("V = right singular vectors (matlab base-1)\n");
+    print_matrix(n, n, V.data(), ldv);
+    std::printf("=====\n");
+
+    /* step 6: measure error of singular value */
+    double ds_sup = 0;
+    for (int j = 0; j < minmn; j++) {
+        double err = fabs(S[j] - S_exact[j]);
+        ds_sup = (ds_sup > err) ? ds_sup : err;
+    }
+    std::printf("|S - S_exact|_sup = %E \n", ds_sup);
+
+    CUSOLVER_CHECK(cusolverDnXgesvdjGetSweeps(cusolverH, gesvdj_params, &executed_sweeps));
+
+    CUSOLVER_CHECK(cusolverDnXgesvdjGetResidual(cusolverH, gesvdj_params, &residual));
+
+    std::printf("residual |A - U*S*V**H|_F = %E \n", residual);
+    std::printf("number of executed sweeps = %d \n", executed_sweeps);
 
     /* free resources */
     CUDA_CHECK(cudaFree(d_A));
