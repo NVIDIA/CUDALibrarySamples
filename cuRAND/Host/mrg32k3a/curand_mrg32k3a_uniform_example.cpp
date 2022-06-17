bc0f6b8439173cacfb14b30195c474a19a13c98b
Matthew Nicely <mnicely@nvidia.com>
2022-06-17 10:16:42 -0700

    Adding cuRAND example for Host API Thanks @gabrielle9talavera @aasthajh for the great work

---
+++ cuRAND/Host/mrg32k3a/curand_mrg32k3a_uniform_example.cpp
@@ -0,0 +1,122 @@
+/*
+ * This program uses the host CURAND API to generate 100
+ * pseudorandom floats.
+ */
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+#include <vector>
+
+#include <cuda_runtime.h>
+#include <curand.h>
+
+#include "curand_utils.h"
+
+using data_type = float;
+
+void run_on_device(const int &n, const unsigned long long &offset,
+                   const unsigned long long &seed,
+                   const curandOrdering_t &order, const curandRngType_t &rng,
+                   const cudaStream_t &stream, curandGenerator_t &gen,
+                   std::vector<data_type> &h_data) {
+
+  data_type *d_data = nullptr;
+
+  /* C data to device */
+  CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_data),
+                        sizeof(data_type) * h_data.size()));
+
+  /* Create pseudo-random number generator */
+  CURAND_CHECK(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_MRG32K3A));
+
+  /* Set cuRAND to stream */
+  CURAND_CHECK(curandSetStream(gen, stream));
+
+  /* Set offset */
+  CURAND_CHECK(curandSetGeneratorOffset(gen, offset));
+
+  /* Set ordering */
+  CURAND_CHECK(curandSetGeneratorOrdering(gen, order));
+
+  /* Set seed */
+  CURAND_CHECK(curandSetPseudoRandomGeneratorSeed(gen, seed));
+
+  /* Generate n floats on device */
+  CURAND_CHECK(curandGenerateUniform(gen, d_data, h_data.size()));
+
+  /* Copy data to host */
+  CUDA_CHECK(cudaMemcpyAsync(h_data.data(), d_data,
+                             sizeof(data_type) * h_data.size(),
+                             cudaMemcpyDeviceToHost, stream));
+
+  /* Sync stream */
+  CUDA_CHECK(cudaStreamSynchronize(stream));
+
+  /* Cleanup */
+  CUDA_CHECK(cudaFree(d_data));
+}
+
+void run_on_host(const int &n, const unsigned long long &offset,
+                 const unsigned long long &seed, const curandOrdering_t &order,
+                 const curandRngType_t &rng, const cudaStream_t &stream,
+                 curandGenerator_t &gen, std::vector<data_type> &h_data) {
+
+  /* Create pseudo-random number generator */
+  CURAND_CHECK(curandCreateGeneratorHost(&gen, CURAND_RNG_PSEUDO_MRG32K3A));
+
+  /* Set cuRAND to stream */
+  CURAND_CHECK(curandSetStream(gen, stream));
+
+  /* Set offset */
+  CURAND_CHECK(curandSetGeneratorOffset(gen, offset));
+
+  /* Set ordering */
+  CURAND_CHECK(curandSetGeneratorOrdering(gen, order));
+
+  /* Set seed */
+  CURAND_CHECK(curandSetPseudoRandomGeneratorSeed(gen, seed));
+
+  /* Generate n floats on host */
+  CURAND_CHECK(curandGenerateUniform(gen, h_data.data(), h_data.size()));
+}
+
+int main(int argc, char *argv[]) {
+
+  cudaStream_t stream = NULL;
+  curandGenerator_t gen = NULL;
+  curandRngType_t rng = CURAND_RNG_PSEUDO_MRG32K3A;
+  curandOrdering_t order = CURAND_ORDERING_PSEUDO_BEST;
+
+  const int n = 10;
+
+  const unsigned long long offset = 0ULL;
+  const unsigned long long seed = 1234ULL;
+
+  /* Create stream */
+  CUDA_CHECK(cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking));
+
+  /* Allocate n floats on host */
+  std::vector<data_type> h_data(n, 0);
+
+  run_on_host(n, offset, seed, order, rng, stream, gen, h_data);
+
+  printf("Host\n");
+  print_vector(h_data);
+  printf("=====\n");
+
+  run_on_device(n, offset, seed, order, rng, stream, gen, h_data);
+
+  printf("Device\n");
+  print_vector(h_data);
+  printf("=====\n");
+
+  /* Cleanup */
+  CURAND_CHECK(curandDestroyGenerator(gen));
+
+  CUDA_CHECK(cudaStreamDestroy(stream));
+
+  CUDA_CHECK(cudaDeviceReset());
+
+  return EXIT_SUCCESS;
+}
---
+++ cuRAND/utils/curand_utils.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <cstring>
+#include <stdexcept>
+#include <vector>
+
+#include <cuda_runtime.h>
+#include <curand.h>
+
+// CUDA API error checking
+#define CUDA_CHECK(err)                                                        \
+  do {                                                                         \
+    cudaError_t err_ = (err);                                                  \
+    if (err_ != cudaSuccess) {                                                 \
+      std::printf("CUDA error %d at %s:%d\n", err_, __FILE__, __LINE__);       \
+      throw std::runtime_error("CUDA error");                                  \
+    }                                                                          \
+  } while (0)
+
+// curand API error checking
+#define CURAND_CHECK(err)                                                      \
+  do {                                                                         \
+    curandStatus_t err_ = (err);                                               \
+    if (err_ != CURAND_STATUS_SUCCESS) {                                       \
+      std::printf("curand error %d at %s:%d\n", err_, __FILE__, __LINE__);     \
+      throw std::runtime_error("curand error");                                \
+    }                                                                          \
+  } while (0)
+
+template <typename T> void print_vector(const std::vector<T> &data);
+
+template <> void print_vector(const std::vector<float> &data) {
+  for (auto &i : data)
+    std::printf("%0.6f\n", i);
+}
+
+template <> void print_vector(const std::vector<unsigned int> &data) {
+  for (auto &i : data)
+    std::printf("%d\n", i);
+}
