a3629bb4323adf6b0414377d6f63474118bb1200
Zan Xu <zanx@nvidia.com>
2025-04-23 14:24:22 -0700

    Update cuFFTMp build configurations

--- cuFFTMp/samples/common.mk
+++ cuFFTMp/samples/common.mk
@@ -10,6 +10,7 @@ MPI         ?= mpi_ibm
 else
 MPI         ?= mpi
 endif
-CXXFLAGS = -std=c++17 --generate-code arch=compute_70,code=sm_70 --generate-code arch=compute_80,code=sm_80 --generate-code arch=compute_90,code=sm_90
+# Also see https://docs.nvidia.com/cuda/cufftmp/usage/requirements.html for supported architectures
+CXXFLAGS = -std=c++17 --generate-code arch=compute_70,code=sm_70 --generate-code arch=compute_75,code=sm_75 --generate-code arch=compute_80,code=sm_80 --generate-code arch=compute_86,code=sm_86 --generate-code arch=compute_89,code=sm_89 --generate-code arch=compute_90,code=sm_90 --generate-code arch=compute_100,code=sm_100
 INCFLAGS = -I${CUFFT_INC} -I${NVSHMEM_INC} -I${MPI_HOME}/include
 LDFLAGS  = -lcuda -L${CUFFT_LIB} -L${NVSHMEM_LIB}  -lcufftMp -lnvshmem_device -lnvshmem_host -L${MPI_HOME}/lib -l${MPI}
