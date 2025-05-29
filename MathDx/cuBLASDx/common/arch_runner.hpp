6444d84b857fb8ff1bc81899cb5aba770a441dd4
Doris Pan <dorisp@nvidia.com>
2025-05-29 13:58:43 -0700

    Add mistatch sm eror message for cuBlasDx and cuFFTTx

--- MathDx/cuBLASDx/common/arch_runner.hpp
+++ MathDx/cuBLASDx/common/arch_runner.hpp
@@ -112,6 +112,11 @@ StatusType arch_runner(unsigned cuda_device_arch, Functor example_functor, Args&
             }
             break;
         }
+        default: {
+            printf("Examples not configured to support SM %u.  Use the CUBLASDX_CUDA_ARCHITECTURES CMake variable to configure the SM support.\n",
+                   cuda_device_arch);
+            return 1;
+        }
     }
 
     // We cannot check invoke_result_t because that
